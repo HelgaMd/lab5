f6f4f7816e75fee5499a74a952760f3ef6e6b160
RomanValeryanov <romich271@mail.ru>
2016-12-24 18:16:20 +0300

    Add files via upload

---
+++ WeatherParser.java
@@ -0,0 +1,11 @@
+package lab5.weather;
+
+
+import com.github.fedy2.weather.data.Channel;
+
+public class WeatherParser {
+	public String parse (Channel channel) {
+        return "The weather in "+channel.getLocation().getCity()
+                + " is " + channel.getItem().getCondition().getTemp() +" "+ channel.getUnits().getTemperature();
+    }
+}
